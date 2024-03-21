e13878aaadd4d7620adc2172bba1fce91650b675
paksa <paksa@parksang>
2024-03-21 09:34:46 +0900

    add : 메인 페이지 컨트롤러 추가

---
+++ projectEtoos/src/main/java/Controller/MainController.java
@@ -0,0 +1,21 @@
+package Controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/Main.do")
+public class MainController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("main.jsp").forward(req, resp);
+	}
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+}
