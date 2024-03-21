1e72610bbb24a151da3e09c7a420069d083b69f3
paksa <paksa@parksang>
2024-03-21 16:28:54 +0900

    add : 세션에 있는 회원 구분값에 따라 다르게 작동되도록 수정

--- projectEtoos/src/main/java/Controller/memberDeleteController.java
+++ projectEtoos/src/main/java/Controller/memberDeleteController.java
@@ -5,21 +5,29 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import member.MemberDAO;
 
 import java.io.IOException;
+
+import org.apache.catalina.Session;
 
 @WebServlet("/memberDelete.do")
 public class memberDeleteController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
 		String id = req.getParameter("id");
-		String referer = req.getHeader("referer");
+		String user_gubun = (String) session.getAttribute("gubun");
 		
 		MemberDAO dao = new MemberDAO();
 		dao.MemberDelete(id);
-		resp.sendRedirect(referer);
+		if(user_gubun.equals("0")) {
+			resp.sendRedirect("/projectEtoos/admin/MemberList.do");
+		}else {
+			resp.sendRedirect("/user/main.do");
+		}
 	}
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
