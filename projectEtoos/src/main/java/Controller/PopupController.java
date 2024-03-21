f35993382a011702dc3ac9950b51ee3c763a95f0
Geeehyun <wkdwl578@gmail.com>
2024-03-21 20:53:39 +0900

    fix : 팝업창 서블릿 추가

---
+++ projectEtoos/src/main/java/Controller/PopupController.java
@@ -0,0 +1,24 @@
+package Controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/AuthChekPopup.do")
+public class PopupController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/user/authCheckPop.jsp").forward(req, resp);
+	}
+
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	} 
+
+}
